1fa190e3665e46ccab3177c611a18174cf0ac119
Game.Play() <gamedotplay0@gmail.com>
2022-05-05 16:22:16 -0400

    Added EmulatorGameMode class.

---
+++ Source/EmulatorViewer/Public/EmulatorGameMode.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmulatorGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMULATORVIEWER_API AEmulatorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
