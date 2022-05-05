307ae60834573e7ac2f70b3ce95ee6a916901c3e
Game.Play() <gamedotplay0@gmail.com>
2022-05-05 16:22:30 -0400

    Added EmulatorPlayerController class.

---
+++ Source/EmulatorViewer/Public/EmulatorPlayerController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EmulatorPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMULATORVIEWER_API AEmulatorPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
