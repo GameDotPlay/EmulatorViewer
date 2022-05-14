39d869eeedfb9f1f4ca256a658c14cf780a36fdc
Game.Play() <gamedotplay0@gmail.com>
2022-05-14 12:23:53 -0400

    New PawnInterface class.

---
+++ Source/EmulatorViewer/Private/PawnInterface.cpp
@@ -0,0 +1,6 @@
+
+
+
+#include "PawnInterface.h"
+
+// Add default functionality here for any IPawnInterface functions that are not pure virtual.
---
+++ Source/EmulatorViewer/Public/PawnInterface.h
@@ -0,0 +1,42 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PawnInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UPawnInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class EMULATORVIEWER_API IPawnInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void MiddleMousePressed() { return; }
+
+	virtual void MiddleMouseReleased() { return; }
+
+	virtual void KeyboardF(FVector Location) { return; }
+
+	virtual void KeyboardEND(float NewZ) { return; }
+
+	virtual void MoveForward(float Value) { return; }
+
+	virtual void MoveRight(float Value) { return; }
+
+	virtual void MouseXAxis(float Value) { return; }
+
+	virtual void MouseYAxis(float Value) { return; }
+
+	virtual void MouseWheelAxis(float Value) { return; }
+};
