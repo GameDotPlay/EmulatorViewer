9e1f56638e2baf3759b105a5fd4488a247418899
Game.Play() <gamedotplay0@gmail.com>
2022-05-20 07:38:32 -0400

    Added PopulateDetailsInterface.

---
+++ Source/EmulatorViewer/Private/PopulateDetailsInterface.cpp
@@ -0,0 +1,6 @@
+
+
+
+#include "PopulateDetailsInterface.h"
+
+// Add default functionality here for any IPopulateDetailsInterface functions that are not pure virtual.
---
+++ Source/EmulatorViewer/Public/PopulateDetailsInterface.h
@@ -0,0 +1,25 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PopulateDetailsInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UPopulateDetailsInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class EMULATORVIEWER_API IPopulateDetailsInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
