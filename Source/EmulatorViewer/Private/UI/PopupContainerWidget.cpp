497249ab66a6a4f1d96609c37e99d479a2bc61d2
Game.Play() <gamedotplay0@gmail.com>
2022-05-25 16:38:43 -0400

    Implemented DragDropOperation functionality.

--- Source/EmulatorViewer/Private/UI/PopupContainerWidget.cpp
+++ Source/EmulatorViewer/Private/UI/PopupContainerWidget.cpp
@@ -4,6 +4,12 @@
 #include "UI/PopupContainerWidget.h"
 #include "Components/CanvasPanel.h"
 #include "Components/Widget.h"
+#include "UI/DragWidget.h"
+
+void UPopupContainerWidget::NativeConstruct()
+{
+
+}
 
 UCanvasPanelSlot* UPopupContainerWidget::AddPopup(UUserWidget* Popup) const
 {
@@ -15,7 +21,24 @@ UCanvasPanelSlot* UPopupContainerWidget::AddPopup(UUserWidget* Popup) const
 	return nullptr;
 }
 
-void UPopupContainerWidget::NativeConstruct()
+bool UPopupContainerWidget::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
 {
+	Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
 
+	UDragWidget* DragWidgetResult = Cast<UDragWidget>(InOperation);
+
+	if (!IsValid(DragWidgetResult))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("DragDrop cast failed."));
+		return false;
+	}
+
+	const FVector2D DragWindowOffset = InGeometry.AbsoluteToLocal(InDragDropEvent.GetScreenSpacePosition());
+	const FVector2D DragWindowOffsetResult = DragWindowOffset - DragWidgetResult->DragOffset;
+
+	DragWidgetResult->WidgetReference->AddToViewport();
+	DragWidgetResult->WidgetReference->SetVisibility(ESlateVisibility::Visible);
+	DragWidgetResult->WidgetReference->SetPositionInViewport(DragWindowOffsetResult, false);
+
+	return true;
 }
--- Source/EmulatorViewer/Public/UI/PopupContainerWidget.h
+++ Source/EmulatorViewer/Public/UI/PopupContainerWidget.h
@@ -21,6 +21,8 @@ public:
 
 	UCanvasPanelSlot* AddPopup(UUserWidget* Popup) const;
 
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+
 protected:
 
 	virtual void NativeConstruct() override;
