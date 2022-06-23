15af30d458ecbdeed6ff4b25878361a7d8de4393
Game.Play() <gamedotplay0@gmail.com>
2022-06-23 09:54:31 -0400

    Create BuildModePawn

---
+++ Source/EmulatorViewer/Public/BuildModePawn.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FloatingPawnBase.h"
+#include "BuildModePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMULATORVIEWER_API ABuildModePawn : public AFloatingPawnBase
+{
+	GENERATED_BODY()
+	
+};
