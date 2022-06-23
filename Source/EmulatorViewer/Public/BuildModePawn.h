71814de93c57a7e719ec2db2727818f529b84c8b
Game.Play() <gamedotplay0@gmail.com>
2022-06-23 15:10:00 -0400

    Update BuildModePawn.h
    Added FBuildModeState enum from PlayerController.

--- Source/EmulatorViewer/Public/BuildModePawn.h
+++ Source/EmulatorViewer/Public/BuildModePawn.h
@@ -1,10 +1,16 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "FloatingPawnBase.h"
 #include "BuildModePawn.generated.h"
+
+UENUM()
+enum class FBuildModeState
+{
+	NotBuilding = 0,
+	Placing = 1,
+	Modifying = 2
+};
 
 /**
  * 
@@ -13,5 +19,11 @@ UCLASS()
 class EMULATORVIEWER_API ABuildModePawn : public AFloatingPawnBase
 {
 	GENERATED_BODY()
-	
+
+public:
+
+protected:
+
+private:
+
 };
