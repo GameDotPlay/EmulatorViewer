4e6f4ebab0e32b2a3d77679c4267b0e6f9b956de
Game.Play() <gamedotplay0@gmail.com>
2023-08-01 19:19:26 -0400

    Added comments.

--- Source/EmulatorViewer/Private/BuildModePawn.cpp
+++ Source/EmulatorViewer/Private/BuildModePawn.cpp
@@ -1,5 +1,8 @@
-
-
-
+/*****************************************************************//**
+ * @file   BuildModePawn.cpp
+ * @brief  Implementation file for BuildModePawn.
+ * 
+ * @author Erich Smith
+ * @date   August 01, 2023
+ *  *********************************************************************/
 #include "BuildModePawn.h"
-
--- Source/EmulatorViewer/Public/BuildModePawn.h
+++ Source/EmulatorViewer/Public/BuildModePawn.h
@@ -11,17 +11,21 @@
 #include "FloatingPawnBase.h"
 #include "BuildModePawn.generated.h"
 
+/** Enum defining the current state the BuildModePawn is in. */
 UENUM()
 enum class FBuildModeState
 {
+	/** Currently not placing/building anything. */
 	NotBuilding = 0,
+
+	/** Currently placing/building something. */
 	Placing = 1,
+
+	/** Currently modifying something. */
 	Modifying = 2
 };
 
-/**
- * 
- */
+/** Class for the BuildModePawn. */
 UCLASS()
 class EMULATORVIEWER_API ABuildModePawn : public AFloatingPawnBase
 {
